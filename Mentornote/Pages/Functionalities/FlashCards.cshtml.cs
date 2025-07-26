2baff4757353917cffc9c3dfe5740fa239dfeb64
Phahose <ekwomnick@gmail.com>
2025-07-26 11:35:01 -0600

    Flashcardfs Design done

---
+++ Mentornote/Pages/Functionalities/FlashCards.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Mentornote.Pages.Functionalities
+{
+    public class FlashCardsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
