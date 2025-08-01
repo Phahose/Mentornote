daec2b97147f2d2c6fe9f9809084d63926c3a1cc
Phahose <ekwomnick@gmail.com>
2025-07-31 22:39:03 -0600

    The Dashboard Pages Started

---
+++ Mentornote/Pages/Start.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Mentornote.Pages.Shared
+{
+    public class StartModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
