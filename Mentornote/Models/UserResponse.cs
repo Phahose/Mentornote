858b64c67b214ad3ee2dc380733768b37c12dd76
Phahose <ekwomnick@gmail.com>
2025-10-04 18:15:51 -0600

    The Practice Test Done and Working

---
+++ Mentornote/Models/UserResponse.cs
@@ -0,0 +1,11 @@
+﻿#nullable disable
+namespace Mentornote.Models
+{
+    public class UserResponse
+    {
+        public int QuestionId { get; set; }
+        public int OptionId { get; set; }
+        public string Answer { get; set; }
+
+    }
+}
--- Mentornote/Pages/Functionalities/PracticeTest.cshtml.cs
+++ Mentornote/Pages/Functionalities/PracticeTest.cshtml.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.BlazorIdentity.Pages.Manage;
+using System.Text.Json;
 
 namespace Mentornote.Pages.Functionalities
 {
@@ -20,8 +21,12 @@ namespace Mentornote.Pages.Functionalities
         public List<Note> NotesList { get; set; } = new();
         public Note Note = new();
         public List<Test> Tests { get; set; } = new();
-        public Test ActiveTest { get; set; } = new Test();  
-
+        public Test ActiveTest { get; set; } = new Test();
+        [BindProperty]
+        public string UserResponses { get; set; }
+        public decimal Score { get; set; } = -1;
+        [BindProperty]
+        public string Submit { get; set; }
 
         public PracticeTestModel(TestServices testServices)
         {
@@ -61,6 +66,48 @@ namespace Mentornote.Pages.Functionalities
 
 
             HttpContext.Session.SetInt32("SelectedNoteId", noteId);
+        }
+
+        public IActionResult OnPost()
+        {
+            // Get all tests for the note
+            int noteId = HttpContext.Session.GetInt32("SelectedNoteId") ?? 0;
+            Tests = cardService.GetTestsWithQuestions(noteId);
+            ActiveTest = Tests.FirstOrDefault();
+            Score = 0;
+
+            if (Submit == "Retake Test")
+            {
+                Score = -1;
+            }
+            else if (!string.IsNullOrEmpty(UserResponses))
+            {
+                var options = new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true
+                };
+
+                var responses = JsonSerializer.Deserialize<List<UserResponse>>(UserResponses, options);
+
+                foreach (var response in responses)
+                {
+                    var question = ActiveTest.Questions.FirstOrDefault(q => q.Id == response.QuestionId); ;
+                    var choices = question.Choices;
+                    var selectedChoice = choices.Where(c => c.Id == response.OptionId).FirstOrDefault();
+                    if (selectedChoice.IsCorrect == true)
+                    {
+                        Score++;
+                    }
+
+                  
+                }
+
+                Score = Math.Round((Score * 100) / ActiveTest.Questions.Count, 2);
+                
+
+            }
+            OnGet(noteId);
+            return Page();
         }
     }
 }
